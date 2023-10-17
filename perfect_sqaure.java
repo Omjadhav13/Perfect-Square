29ba41bc32f2dddd5fb9615ef7252c90157afd28
Om Jadhav <74755805+Omjadhav13@users.noreply.github.com>
2023-10-17 19:37:20 +0530

    Update perfect_sqaure.java

--- perfect_sqaure.java
+++ perfect_sqaure.java
@@ -1,19 +1,18 @@
-package spiders;
 
-import java.util.Scanner;
-
+import java.util.*;
 public class perfect_sqaure {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
+		int a=sc.nextInt(); //input form the user
 		int i=1;
 		for( ;i*i<=a;i++) {
 			if(i*i==a) {
-				System.out.println("perfect");
+				System.out.println("The number " + a + "is perfect square");
 				break;
 			}
 		}
 		if(i*i>a) {
-			System.out.println("not a perfect sqaure");
+			System.out.println("The number " + a + "is not a perfect square");
 		}
-}}
+	}
+}
